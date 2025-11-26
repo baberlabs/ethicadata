2d0dbe062774c48a7239eaf81404d7f407b3a96f
Baber Khan <baber.khan@mail.bcu.ac.uk>
2025-11-26 11:50:54 +0000

    Work during lab 10 (26 Nov 2025)

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,14 @@
 const navContainerMB = document.querySelector(".nav-container-mb");
 const menuIcon = document.querySelector(".menu-icon");
+const searchInput = document.querySelector(".search-bar > input");
+const searchButton = document.querySelector(".search-bar > button");
+
+searchButton.addEventListener("click", () => {
+  const query = searchInput.value.trim();
+  alert(
+    `Unfortunately, the search function is not yet implemented. You searched for: "${query}"`
+  );
+});
 
 menuIcon.addEventListener("click", () => {
   navContainerMB.classList.toggle("nav-open");
